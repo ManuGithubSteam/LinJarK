8ac25be5ba30f1f6ed360666d30c1c7e41427e86
ManuGithubSteam <manuelsoukup@googlemail.com>
2015-10-28 00:37:14 +0100

    fixes

--- data/Simon_repair_training_data.sh
+++ data/Simon_repair_training_data.sh
@@ -16,167 +16,11 @@ find . -type f -print0 | perl -n0e '$new = $_; if($new =~ s/[^[:ascii:]]/#/g) { 
 
 # very very very slow when we got many files!
 
+cd ~/.kde4/share/apps/simon/model
 
-for f in *; do
-mv $f $(echo $f | sed -e 's/_####ffne/_öffne/g');done
+python fix_names.py
 
-for f in *; do
-mv $f $(echo $f | sed -e 's/_##ffne/_öffne/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/schlie####en/schließen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/schlie##en/schließen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/einf####gen/einfügen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/einf##gen/einfügen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_l####schen_/_löschen_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_l##schen_/_löschen_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Empf####nger/Empfänger/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Ausw####hlen/Auswählen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Empf##nger/Empfänger/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Ausw##hlen/Auswählen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/N####chste/Nächste/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/N##chste/Nächste/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/n####chstes/nächstes/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/n##chstes/nächstes/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/R####ckg####ngig/Rückgängig/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/R##ckg##ngig/Rückgängig/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/gr########er/größer/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/gr####er/größer/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_####bersetze_/_übersetze_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_##bersetze_/_übersetze_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/####bersetzung/Übersetzung/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/##bersetzung/Übersetzung/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Arbeitsfl####che/Arbeitsfläche/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Arbeitsfl##che/Arbeitsfläche/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/zuf####llig/zufällig/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/zuf##llig/zufällig/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/F####nf/Fünf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/F##nf/Fünf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Zw####lf/Zwölf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Zw##lf/Zwölf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/####bermorgen/übermorgen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/##bermorgen/übermorgen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_f####r_/_für_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/_f##r_/_für_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/f####nf/fünf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/f##nf/fünf/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/st####rke/stärke/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/st##rke/stärke/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Windverh####ltnisse/Windverhältnisse/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Windverh##ltnisse/Windverhältnisse/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/n####chste/nächste/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Abk####rzung/Abkürzung/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Abk##rzung/Abkürzung/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/l####uft_/läuft_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/l##uft_/läuft_/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Einf####gen/Einfügen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Einf##gen/Einfügen/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Zur####ck/Zurück/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Zur##ck/Zurück/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Franz####sisch/Französisch/g');done
-
-for f in *; do
-mv $f $(echo $f | sed -e 's/Franz##sisch/Französisch/g');done
-
-
+cd ~/.kde4/share/apps/simon/model/training.data
 ###
 ls -1 | sed 's/.wav//g' | sed 's/$/ "default" /' > ../1.txt
 
@@ -194,3 +38,5 @@ cat 3.txt | sed 's/\t//g' > prompts
 rm 1.txt
 rm 2.txt
 rm 3.txt
+
+kdialog --caption "Bitte warten" --title "Wichtig" --msgbox "Fertig. Bitte Simon neu starten."
---
+++ data/fix_names.py
@@ -0,0 +1,191 @@
+#!/bin/python
+# -*- coding: iso-8859-1 -*-
+import os
+
+os.chdir("training.data")
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("N####chste", "Nächste"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("N##chste", "Nächste"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("####ffne", "öffne"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("##ffne", "öffne"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("schlie####e", "schließe"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("schlie##e", "schließe"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("einf####gen", "einfügen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("einf##gen", "einfügen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("l####sche", "lösche"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("l##sche", "lösche"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Empf####nger", "Empfänger"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Empf##nger", "Empfänger"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Ausw####hlen", "Auswählen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Ausw##hlen", "Auswählen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("n####chste", "nächste"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("n##chste", "nächste"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("R####ckg####ngig", "Rückgängig"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("R##ckg##ngig", "Rückgängig"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("gr########er", "größer"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("gr####er", "größer"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("####bersetze", "übersetze"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("##bersetze", "übersetze"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("####bersetzung", "Übersetzung"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("##bersetzung", "Übersetzung"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("fl####che", "fläche"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("fl##che", "fläche"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("zuf####llig", "zufällig"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("zuf##llig", "zufällig"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("F####nf", "Fünf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("F##nf", "Fünf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("f####nf", "fünf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("f##nf", "fünf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Zw####lf", "Zwölf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Zw##lf", "Zwölf"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("####ber", "über"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("##ber", "über"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("f####r", "für"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("f##r", "für"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("st####rke", "stärke"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("st##rke", "stärke"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("verh####ltnisse", "verhältnisse"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("verh##ltnisse", "verhältnisse"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Abk####rzung", "Abkürzung"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Abk##rzung", "Abkürzung"))
+    
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("l####uft", "läuft"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("l##uft", "läuft"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Einf####gen", "Einfügen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Einf##gen", "Einfügen"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Zur####ck", "Zurück"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Zur##ck", "Zurück"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Franz####sisch", "Französisch"))
+
+for fileName in os.listdir("."):
+    os.rename(fileName, fileName.replace("Franz##sisch", "Französisch"))
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
