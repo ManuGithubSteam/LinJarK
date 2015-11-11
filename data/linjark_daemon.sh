9f02b6a2c34797ab260292006cb5c652b7dff797
ManuGithubSteam <manuelsoukup@gmail.com>
2015-11-11 12:50:56 +0100

    new pi stuff

--- data/linjark_daemon.sh
+++ data/linjark_daemon.sh
@@ -6,14 +6,53 @@ cd /tmp
 
 touch autostart_working
 
+ON_PI="no"
+
 while true; do
+
+# Housekeeping
 sleep 1
 
+if [ "$ON_PI" == "yes" ]
+  then
 
+
+ps cax | grep simond
+if [ $? -ne 0 ]
+  then
+
+  killall simon
+  simond &
+  #ksimond &
+  
+ fi
+ps cax | grep simon | grep Sl
+if [ $? -ne 0 ]
+  then
+
+  simon &
+  
+ fi
+ 
+ fi
+###### Ende
+
+
+##### WETTER
+###################
 if [ -f ~/.linjark/scripts/weather/linjark_weather ]
 then
 
-cd /tmp
+
+# timer 
+if [ ! -f  ~/.linkark/.weather_update ] then
+echo "timestampfile" > ~/.linkark/.weather_update
+fi
+
+if [ "$(( $(date +"%s") - $(stat -c "%Y" ~/.linkark/.weather_update) ))" -gt "7200" ]; then
+   echo "is older then 2 hours"
+   
+   cd /tmp
 
 GETCITY=$(cat ~/.linjark/scripts/weather/linjark_weather | grep MYCITY_URL= | tail -n1 | cut -d'"' -f2)
 MYCITY=$(echo $GETCITY | sed 's/http\:\/\/www.wetter.de\/deutschland\///'| sed 's/.html//g')
@@ -24,10 +63,22 @@ MYCITY=$(echo $GETCITY | sed 's/http\:\/\/www.wetter.de\/deutschland\///'| sed '
   wget http://www.wetter.de/deutschland/$MYCITY/wetterbericht-aktuell.html -O wetterbericht-1.html &
   wget http://www.wetter.de/deutschland/$MYCITY/wetter-bericht.html -O wetterbericht-4.html &
   
- fi
- 
- 
+  rm ~/.linkark/.weather_update
   
-   sleep 10m
+fi
+  
+ fi
+ ##### Ende
+###################
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 done
